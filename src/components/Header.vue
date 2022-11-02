d5155b314a59b3ea1e36fd31ebb04ac255d21971
Victor Hugo Aguilar <victorhugoaguilar@aol.com>
2022-11-02 09:32:58 +0100

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <h2>{{title}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String
+    },
+    
+    data(){
+        return {
+            title = this.title;
+        }
+    }
+}
+</script>
+
+<style scoped>
+:host {
+    display: block;
+}
+</style>
