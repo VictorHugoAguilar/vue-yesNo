9cb5141c641b75f35961a6a88080df879726f9e7
Victor Hugo Aguilar <victorhugoaguilar@aol.com>
2022-11-02 09:38:39 +0100

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <h3>{{title}}</h3>
+        <h5>{{description}}</h5>
+    </div>
+</template>
+<script>
+export default{
+    props:{
+        title: { type: String, default: 'Footer' },
+        description: { type: String, default: 'Version 1.0' }
+    },
+    data(){
+        title: this.title;
+        description: this.description;
+    },
+}
+</script>
+
+<style scoped>
+:host {
+    display: block;
+}
+</style>
+
+
+
